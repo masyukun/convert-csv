9c2764f554090902ec3fad38c88c26d55fb0be80
Matthew Royal <masyukun@gmail.com>
2015-11-18 12:25:39 -0800

    Relocate triple tools into SemTriple

--- src/main/java/com/matthewroyal/marklogic/semantics/SemTriple.java
+++ src/main/java/com/matthewroyal/marklogic/semantics/SemTriple.java
@@ -1,12 +1,47 @@
 package com.matthewroyal.marklogic.semantics;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
+
+import org.apache.commons.csv.CSVRecord;
+
+import com.matthewroyal.marklogic.RDBSchemaModel.RDBSchemaModel;
 
 public class SemTriple {
 
 	public static final String SEM = "sem";
 	public static final String SEM_TRIPLE_NAMESPACE = "http://marklogic.com/semantics";
+
+	// RDB namespaces
+	public static String NAMESPACE_ROOT = "http://matthewroyal.com/ConvertRDB/";
+	public static String RDB = NAMESPACE_ROOT + "rdb#";
+	public static String DB = NAMESPACE_ROOT + "db#";
+	public static String FK = NAMESPACE_ROOT + "foreignKey#";
+	
+	/**
+	 * Use this namespace when referring to the table itself
+	 * @param dbName
+	 * @param tableName
+	 * @return
+	 */
+	public static String TABLE_NAMESPACE(String dbName, String tableName) {
+		return String.format("%s%s/table#%s", NAMESPACE_ROOT, dbName, tableName);
+	}
+	
+	/**
+	 * Use this namespace when referring to specific data from a table
+	 * @param dbName
+	 * @param tableName
+	 * @return
+	 */
+	public static String TABLEDATA_NAMESPACE(String dbName, String tableName) {
+		return String.format("%s%s/%s", NAMESPACE_ROOT, dbName, tableName);
+	}
+	
 	
 	public String subject = null;
 	public String predicate = null;
@@ -35,6 +70,26 @@ public class SemTriple {
 	 */
 	public static void writeTriple(XMLStreamWriter xMLStreamWriter, SemTriple triple, Boolean solitary) throws XMLStreamException {
 		writeTriple(xMLStreamWriter, triple.subject, triple.predicate, triple.object, solitary);
+	}
+
+	public static void writeTriple(XMLStreamWriter xMLStreamWriter, List<SemTriple> triple, Boolean solitary) throws XMLStreamException {
+
+		if (null != triple && triple.size() > 0) {
+			// Group the collection of triples in their own triples element
+			if (solitary) {
+				xMLStreamWriter.writeCharacters("\n  ");
+		        xMLStreamWriter.writeStartElement(SEM, "triples", SEM_TRIPLE_NAMESPACE);
+		        xMLStreamWriter.writeNamespace(SEM, SEM_TRIPLE_NAMESPACE);			
+			}
+			for (SemTriple t : triple) {
+				// Lie to the next method down about solitariness -- it's handled for collections right here
+				writeTriple(xMLStreamWriter, t, false);			
+			}
+		    if (solitary) {
+		        xMLStreamWriter.writeCharacters("\n  ");
+			    xMLStreamWriter.writeEndElement();
+		    }
+		}
 	}
 	
 	/**
@@ -85,7 +140,5 @@ public class SemTriple {
 		    xMLStreamWriter.writeEndElement();
 	    }
 	}
-
-
 
 }
